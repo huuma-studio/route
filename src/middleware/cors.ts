c55cc8ee80183bbbcacc18b8b723f7084ed8e0b5
Daniel Steuri <daniel.steuri@flowingones.ch>
2024-01-28 22:21:05 +0100

    feat: add dedicated cors middleware

---
+++ src/middleware/cors.ts
@@ -0,0 +1,37 @@
+import { HttpMethod } from "../http/http-method.ts";
+import { RequestContext } from "../http/request.ts";
+import { Middleware, Next } from "./middleware.ts";
+
+interface CorsOptions {
+  allowedCorsDomains?: string[];
+  allowedCorsHeaders?: string[];
+  allowedCorsMethods?: HttpMethod[];
+}
+
+export function Cors(
+  { allowedCorsDomains, allowedCorsHeaders, allowedCorsMethods }: CorsOptions,
+): Middleware {
+  return async (ctx: RequestContext, next: Next) => {
+    const origin = ctx.request.headers.get("origin");
+    const resp = await next(ctx);
+    if (origin && allowedCorsDomains?.includes(origin)) {
+      resp.headers.set(
+        "Access-Control-Allow-Origin",
+        origin,
+      );
+      if (allowedCorsHeaders) {
+        resp.headers.set(
+          "Access-Control-Allow-Headers",
+          allowedCorsHeaders.join(", "),
+        );
+      }
+      if (allowedCorsMethods) {
+        resp.headers.set(
+          "Access-Control-Allow-Methods",
+          allowedCorsMethods.join(", "),
+        );
+      }
+    }
+    return resp;
+  };
+}
